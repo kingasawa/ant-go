961ac795aa193cacb263b04b54f58b3da914dc36
khanhtranicd <khanhtc@icd-vn.com>
2026-04-28 16:28:53 +0700

    fix ts

--- app/account/app/[appName]/page.tsx
+++ app/account/app/[appName]/page.tsx
@@ -1,6 +1,11 @@
 import { redirect } from "next/navigation";
 
-export default function AppPage({ params }: { params: { appName: string } }) {
-  redirect(`/account/app/${params.appName}/app-info`);
+export default async function AppPage({
+  params,
+}: {
+  params: Promise<{ appName: string }>;
+}) {
+  const { appName } = await params;
+  redirect(`/account/app/${appName}/app-info`);
 }
 
